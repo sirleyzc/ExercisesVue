7adfbd5cf74a1388b6fce272abd3df23a8fe95b9
Sirley Zuñiga Cerquera <szuniga16@misena.edu.co>
2021-08-19 19:15:09 -0500

    Ejercicios operaciones básicas

---
+++ js/operaciones.js
@@ -0,0 +1,27 @@
+var operaciones = new Vue({
+    el: '#app',
+    data: {
+        num1: null,
+        num2: null,
+        rta: null
+        },
+        
+    methods: {
+      add(){
+          this.rta = this.num1 + this.num2;
+      },
+      substract(){
+        this.rta = this.num1 - this.num2;
+      },
+      multiply(){
+        this.rta = this.num1 * this.num2;
+      },
+      divide(){
+          if (this.num2 == 0) {
+              this.rta = "Error, no se puede dividir por cero"
+          } else {
+              this.rta = this.num1 / this.num2;
+          }
+      }
+    },
+  });
